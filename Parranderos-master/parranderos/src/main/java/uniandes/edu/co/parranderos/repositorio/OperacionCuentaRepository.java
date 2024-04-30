98b3500fde9369d4077914f3bd72d1384fc10b77
GabrielAristizabal <g.aristizabal@uniandes.edu.co>
2024-04-29 20:04:26 -0500

    v3

--- Parranderos-master/parranderos/src/main/java/uniandes/edu/co/parranderos/repositorio/OperacionCuentaRepository.java
+++ Parranderos-master/parranderos/src/main/java/uniandes/edu/co/parranderos/repositorio/OperacionCuentaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,10 +24,17 @@ public interface OperacionCuentaRepository extends JpaRepository<Cuenta, Integer
         }
 
         @Modifying
-        @Transactional
+        @Transactional(isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
         @Query(value = "INSERT INTO operacioncuenta (ID, TIPOPAGO, NUMEROCUENTAAFECTADA, NUMEROCUENTA) VALUES (:ID, :TIPOPAGO, :NUMEROCUENTAAFECTADA, :NUMEROCUENTA);", nativeQuery = true)
-        void insertarOperacionCuenta(@Param("ID") Integer ID, @Param("TIPOPAGO") String TIPOPAGO, @Param("NUMEROCUENTAAFECTADA") Integer NUMEROCUENTAAFECTADA,
+        void insertarOperacionCuentaSE(@Param("ID") Integer ID, @Param("TIPOPAGO") String TIPOPAGO, @Param("NUMEROCUENTAAFECTADA") Integer NUMEROCUENTAAFECTADA,
                         @Param("NUMEROCUENTA") Integer NUMEROCUENTA);
+
+        @Modifying
+        @Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
+        @Query(value = "INSERT INTO operacioncuenta (ID, TIPOPAGO, NUMEROCUENTAAFECTADA, NUMEROCUENTA) VALUES (:ID, :TIPOPAGO, :NUMEROCUENTAAFECTADA, :NUMEROCUENTA);", nativeQuery = true)
+        void insertarOperacionCuentaRC(@Param("ID") Integer ID, @Param("TIPOPAGO") String TIPOPAGO, @Param("NUMEROCUENTAAFECTADA") Integer NUMEROCUENTAAFECTADA,
+                        @Param("NUMEROCUENTA") Integer NUMEROCUENTA);
+
         @Query(value = "SELECT OC.ID, OC.TIPOPAGO FROM OPERACIONCUENTA OC WHERE OC.NUMEROCUENTA = :NUMEROCUENTA", nativeQuery = true)
         Collection<RespuestaInformacionCuenta> darInformacionOperacionesCuenta();
 }
