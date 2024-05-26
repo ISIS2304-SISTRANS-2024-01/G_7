a425ae71f093de51f38c2f0c3b5c8c718ba4f88a
Jd-briceno <69830655+Jd-briceno@users.noreply.github.com>
2024-05-26 00:05:05 -0500

    actualizacion

--- aplicacion/src/main/java/com/example/demo/controller/CuentaController.java
+++ aplicacion/src/main/java/com/example/demo/controller/CuentaController.java
@@ -1,41 +1,146 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import lombok.extern.slf4j.Slf4j;
+
+
 import com.example.demo.modelo.Cuenta;
+import com.example.demo.modelo.OperacionesCuenta;
+import com.example.demo.modelo.PuntoAtencion;
 import com.example.demo.repositorio.CuentaRepository;
 
-@RestController
+@Slf4j
+@Controller
 public class CuentaController 
 {
 
     @Autowired
     private CuentaRepository cuentaRepository;
 
-    @PostMapping("/cuentas")
-    public String insertarCuenta(@RequestBody Cuenta cuenta) // no se si RequestBody o ModelAttribute
-    {
-        cuentaRepository.insertarCuenta(cuenta.getTipo(), cuenta.getEstado(), cuenta.getNumeroCuenta(), cuenta.getSaldo(), cuenta.getFecha_ultima_transaccion(), cuenta.getFecha_creaccion());
+    // Crear Punto Atencion
 
-        return "redirect:/cuenta/new";
+
+    public Cuenta obtenerCuentaPorNumero(String numeroCuenta) 
+    {
+        return cuentaRepository.findByNumeroCuenta(numeroCuenta);
+    }
+
+    
+
+    // Crear Cuenta
+
+    @PostMapping("/cuenta/new/save")
+    public String insertarCuenta(@RequestBody Cuenta cuenta) 
+    {
+        try 
+        {
+            if (cuenta.getOperaciones_cuenta() == null) 
+            {
+                cuenta.setOperaciones_cuenta(new ArrayList<>());
+            }
+            cuentaRepository.save(cuenta);
+            return "confirmacionCuentaCreada";
+        } 
+        catch (Exception e) 
+        {
+            log.error("Error al insertar la cuenta", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error al insertar la cuenta", e);
+        }
+    }
+
+    @GetMapping("/cuenta/new")
+    public String cuentaForm(Model model) 
+    {
+        model.addAttribute("cuenta", new Cuenta());
+        return "cuentaNueva";
+    }
+
+
+    // Cerrar cuenta
+
+    @GetMapping("/cuenta/closed")
+    public String cerrarCuenta(Model model)
+    {
+        model.addAttribute("numeroCuenta", new Cuenta());
+        return "cuentaCerrada";
     }
 
     @PostMapping("/operacioncuenta/{numeroCuenta}/close")
-    public String operacionCerrarCuenta(@PathVariable("numeroCuenta") String numeroCuenta)
+    public String operacionCerrarCuenta(@PathVariable("numeroCuenta") String numeroCuenta) 
     {
-        cuentaRepository.CerrarCuenta(numeroCuenta);
+        cuentaRepository.cerrarCuenta(numeroCuenta);
         return "confirmacionCuentaCerrada";
     }
 
-    @PostMapping("/operacioncuenta/{numeroCuenta}/deactivate")
-    public String operacionDesactivarCuenta(@PathVariable("numeroCuenta") String numeroCuenta)
+    // Desactivar cuenta
+
+    @GetMapping("/cuenta/deactivated")
+    public String desactivarCuenta(Model model)
     {
-        cuentaRepository.DesactivarCuenta(numeroCuenta);
+        model.addAttribute("numeroCuenta", new Cuenta());
+        return "cuentaDesactivada";
+    }
+
+    @PostMapping("/operacioncuenta/{numeroCuenta}/deactivate")
+    public String operacionDesactivarCuenta(@PathVariable("numeroCuenta") String numeroCuenta) 
+    {
+        cuentaRepository.desactivarCuenta(numeroCuenta);
         return "confirmacionCuentaDesactivada";
     }
 
+
+
+
+    //Insertar Punto Atencion
+    
+    @GetMapping("/punto/new")
+    public String puntoForm(Model model) {
+        model.addAttribute("puntoAtencion", new PuntoAtencion());
+        return "crearPuntoAtencion";
+    }
+
+    // @PostMapping("/cuenta/{idCuenta}/operacion/{tipoOperacion}/puntoatencion/agregar")
+    // public String insertarPuntoAtencion(@ModelAttribute PuntoAtencion puntoAtencion, @PathVariable String idCuenta, @PathVariable String tipoOperacion) 
+    // {
+    //     cuentaRepository.insertarPuntoAtencion(tipoOperacion, 1);
+    //     return "redirect:/confirmacionPuntoCreado";
+    // }
+
+
+    @PostMapping("/cuenta/{idCuenta}/operacion/{tipoOperacion}/puntoatencion/agregar")
+    public String insertarPuntoAtencion(@RequestParam("tipo") String tipo, @RequestParam("operaciones_validas") List<String> operacionesValidas, @RequestParam("oficina") int oficina)
+    {
+        // Crea un objeto PuntoAtencion con los parámetros recibidos
+        PuntoAtencion puntoAtencion = new PuntoAtencion();
+        puntoAtencion.setTipo(tipo);
+        puntoAtencion.setOperaciones_validas(operacionesValidas);
+        puntoAtencion.setOficina(oficina);
+
+    // Aquí deberías tener lógica para asociar el punto de atención con la cuenta específica
+        cuentaRepository.insertarPuntoAtencion(puntoAtencion);
+        return "redirect:/confirmacionPuntoCreado";
+    }
+
+
+    @GetMapping("/confirmacionPuntoCreado")
+    public String confirmacionPuntoCreado() 
+    {
+        return "confirmacionPuntoCreado";
+    }
 }
--- aplicacion/src/main/java/com/example/demo/repositorio/CuentaRepository.java
+++ aplicacion/src/main/java/com/example/demo/repositorio/CuentaRepository.java
@@ -1,24 +1,33 @@
 package com.example.demo.repositorio;
 
-import java.sql.Date;
+import java.util.Date;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
 
 import com.example.demo.modelo.Cuenta;
+import com.example.demo.modelo.PuntoAtencion;
 
 public interface CuentaRepository extends MongoRepository<Cuenta, String>
 {
-    @Query("{tipo: ?0, estado: ?1, numeroCuenta: ?2, saldo: ?3, fecha_ultima_transaccion: ?4, fecha_creaccion: ?5, operaciones_cuenta: [ null ] }")
-    void insertarCuenta(String tipo, String estado, String numeroCuenta, float saldo, Date fecha_ultima_transaccion, Date fecha_creaccion);
+
+    @Query("{numeroCuenta: ?0}")
+    Cuenta findByNumeroCuenta(String numeroCuenta);
+
+    @Query("{tipo: ?0, estado: ?1, numeroCuenta: ?2, saldo: ?3, fecha_ultima_transaccion: ?4, fecha_creacion: ?5, operaciones_cuenta: [ null ] }")
+    Cuenta insertarCuenta(String tipo, String estado, String numeroCuenta, int saldo, Date fecha_ultima_transaccion, Date fecha_creacion);
 
     @Query("{numeroCuenta: ?0}")
     @Update("{ '$set': { estado: 'Cerrada' }}")
-    void CerrarCuenta(String numeroCuenta);
+    void cerrarCuenta(String numeroCuenta);
       
     @Query("{numeroCuenta: ?0}")
     @Update("{ '$set': { estado: 'Desactivada' }}")
-    void DesactivarCuenta(String numeroCuenta);
+    void desactivarCuenta(String numeroCuenta);
+
+    @Query("{'numeroCuenta': '123' }, { $push: { 'operaciones_cuenta.$.punto_atencion': ?0 } }")
+    PuntoAtencion insertarPuntoAtencion(PuntoAtencion puntoAtencion);
+
 
 }
