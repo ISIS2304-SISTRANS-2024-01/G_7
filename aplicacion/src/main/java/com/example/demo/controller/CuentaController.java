1b5e0fae79f14fc483fc5ff45b14805fbbfa4bd6
Jd-briceno <69830655+Jd-briceno@users.noreply.github.com>
2024-05-26 21:46:48 -0500

    P4

--- aplicacion/src/main/java/com/example/demo/controller/CuentaController.java
+++ aplicacion/src/main/java/com/example/demo/controller/CuentaController.java
@@ -2,26 +2,24 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 import com.example.demo.modelo.Cuenta;
-import com.example.demo.modelo.OperacionesCuenta;
 import com.example.demo.modelo.PuntoAtencion;
 import com.example.demo.repositorio.CuentaRepository;
 
@@ -110,7 +108,8 @@ public class CuentaController
     //Insertar Punto Atencion
     
     @GetMapping("/punto/new")
-    public String puntoForm(Model model) {
+    public String puntoForm(Model model) 
+    {
         model.addAttribute("puntoAtencion", new PuntoAtencion());
         return "crearPuntoAtencion";
     }
@@ -126,21 +125,139 @@ public class CuentaController
     @PostMapping("/cuenta/{idCuenta}/operacion/{tipoOperacion}/puntoatencion/agregar")
     public String insertarPuntoAtencion(@RequestParam("tipo") String tipo, @RequestParam("operaciones_validas") List<String> operacionesValidas, @RequestParam("oficina") int oficina)
     {
-        // Crea un objeto PuntoAtencion con los parámetros recibidos
         PuntoAtencion puntoAtencion = new PuntoAtencion();
         puntoAtencion.setTipo(tipo);
         puntoAtencion.setOperaciones_validas(operacionesValidas);
         puntoAtencion.setOficina(oficina);
 
-    // Aquí deberías tener lógica para asociar el punto de atención con la cuenta específica
         cuentaRepository.insertarPuntoAtencion(puntoAtencion);
         return "redirect:/confirmacionPuntoCreado";
     }
 
-
     @GetMapping("/confirmacionPuntoCreado")
-    public String confirmacionPuntoCreado() 
+    public String mostrarConfirmacionPuntoCreado() 
     {
         return "confirmacionPuntoCreado";
+    }
+
+
+    //Borrar Punto Atencion
+
+    @PostMapping("/cuenta/{idCuenta}/operacion/{tipoOperacion}/puntoatencion/eliminar")
+    public String borrarPuntoAtencion(@PathVariable("idCuenta") String idCuenta, @PathVariable("tipoOperacion") String tipoOperacion, @RequestParam("oficina") int oficina) 
+    {
+        cuentaRepository.borrarPuntoAtencion(oficina);
+        return "redirect:/confirmacionPuntoBorrado";
+    }
+
+    @GetMapping("/puntoatencion/eliminar")
+    public String confirmacionPuntoBorrado() 
+    {
+        return "borrarPuntoAtencion";
+    }
+
+    @GetMapping("/confirmacionPuntoBorrado")
+    public String mostrarConfirmacionPuntoBorrado() 
+    {
+        return "borrarPuntoAtencion";
+    }
+
+
+    //Consignar
+
+    @GetMapping("/operacioncuenta/consignar")
+    public String cuentaConsignar(Model model)
+    {
+        return "consignar";
+    }
+
+    @PostMapping("/consignar")
+    public String consignarDinero(@RequestParam("cuentaDestino") String numeroCuenta, @RequestParam("monto") int valor, RedirectAttributes redirectAttributes) 
+    {
+        try 
+        {
+            cuentaRepository.sumarSaldoDestino(numeroCuenta, valor);
+            redirectAttributes.addFlashAttribute("mensaje", "Consignación realizada exitosamente");
+        } 
+        catch (Exception e) 
+        {
+            redirectAttributes.addFlashAttribute("mensaje", "Error al realizar la consignación: " + e.getMessage());
+            return "redirect:/confirmacionConsignacion";
+        }
+    return "redirect:/confirmacionConsignacion";
+    }
+
+    @GetMapping("/confirmacionConsignacion")
+    public String mostrarConfirmacionConsignacion() 
+    {
+        return "confirmacionConsignacion";
+    }
+
+    //Retirar
+    
+
+    @GetMapping("/operacioncuenta/retirar")
+    public String cuentaRetirar(Model model)
+    {
+        model.addAttribute("numerocuenta", new Cuenta());
+        return "retirar";
+    }
+
+    @PostMapping("/retirar")
+    @Transactional
+    public String retirarDinero(@RequestParam("cuentaDestino") String numeroCuenta, @RequestParam("monto") int valor, RedirectAttributes redirectAttributes) 
+    {
+        try 
+        {
+            cuentaRepository.restarSaldoDestino(numeroCuenta, valor);
+            redirectAttributes.addFlashAttribute("mensaje", "Retiro realizado exitosamente");
+        } 
+        catch (Exception e) 
+        {
+            redirectAttributes.addFlashAttribute("mensaje", "Error al realizar el retiro: " + e.getMessage());
+            return "redirect:/confirmacionRetiro";
+        }
+
+        return "redirect:/confirmacionRetiro";
+    }
+
+    @GetMapping("/confirmacionRetiro")
+    public String mostrarConfirmacionRetiro() 
+    {
+        return "confirmacionRetiro";
+    }
+
+
+    //Transferir
+
+    @GetMapping("/operacioncuenta/transferir")
+    public String cuentaTransferir(Model model)
+    {
+        model.addAttribute("numerocuenta", new Cuenta());
+        return "transferir";
+    }
+
+    @PostMapping("/transferir")
+    @Transactional
+    public String transferirDinero(@RequestParam("cuentaOrigen") String numeroCuentaOrigen, @RequestParam("cuentaDestino") String numeroCuentaDestino, @RequestParam("monto") int valor, RedirectAttributes redirectAttributes) 
+    {
+        try 
+        {
+            cuentaRepository.restarSaldoTransferir(numeroCuentaOrigen, numeroCuentaDestino, (valor)*-1);
+            cuentaRepository.sumarSaldoTransferir(numeroCuentaDestino, numeroCuentaOrigen,  valor);
+        } 
+        catch (Exception e) 
+        {
+            redirectAttributes.addFlashAttribute("mensaje", "Error al realizar el retiro: " + e.getMessage());
+            return "redirect:/confirmacionTransferencia";
+        }
+
+        return "redirect:/confirmacionTransferencia";
+    }
+
+    @GetMapping("/confirmacionTransferencia")
+    public String mostrarConfirmacionTransferencia() 
+    {
+        return "confirmacionTransferencia";
     }
 }
--- aplicacion/src/main/java/com/example/demo/repositorio/CuentaRepository.java
+++ aplicacion/src/main/java/com/example/demo/repositorio/CuentaRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repositorio;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -29,5 +30,33 @@ public interface CuentaRepository extends MongoRepository<Cuenta, String>
     @Query("{'numeroCuenta': '123' }, { $push: { 'operaciones_cuenta.$.punto_atencion': ?0 } }")
     PuntoAtencion insertarPuntoAtencion(PuntoAtencion puntoAtencion);
 
+    // @Query("{'numeroCuenta': '123' }") 
+    // @Update("$push: {'operaciones_cuenta.$[].punto_atencion': {tipo: 'digital', operaciones_validas: ['consignar', 'retirar'],oficina: 1}}") // { upsert: false }
+    // void insertarPuntoAtencion(PuntoAtencion puntoAtencion);
 
+    @Query("{ 'numeroCuenta': '123', 'operaciones_cuenta._id': 1 }")
+    @Update("{ $pull: { 'operaciones_cuenta.$.punto_atencion': { 'tipo': 'digital', 'oficina': ?0 } } }")
+    void borrarPuntoAtencion(int oficina);
+
+    @Query(("{'numeroCuenta': ?0 }"))
+    @Update("{$push: { 'operaciones_cuenta': { _id: 4, tipo: 'consignar', cuenta_afectada: ?0, valor: ?1, fecha: new Date(), punto_atencion: []}}, $inc: { 'saldo': ?1 }, $set: { 'fecha_ultima_transaccion': new Date() }})")
+    void sumarSaldoDestino(String numeroCuenta, int valor);
+
+    // @Query("{ 'numeroCuenta': ?0 }")
+    // @Update("{$push: { 'operaciones_cuenta': {_id: 5, tipo: 'retirar', cuenta_afectada: ?0, valor: -?1, fecha: new Date(), punto_atencion: [] }}, $inc: { 'saldo': -?1 }, $set: { 'fecha_ultima_transaccion': new Date() }}")
+    // void restarSaldoDestino(String numeroCuenta, int valor);
+
+    @Query("{ 'numeroCuenta': ?0 }")
+    @Update("{$push: { 'operaciones_cuenta': {_id: 5, tipo: 'retirar', cuenta_afectada: ?0, valor: ?1, fecha: new Date(), punto_atencion: [] }}, $inc: { 'saldo': ?1 }, $set: { 'fecha_ultima_transaccion': new Date() }}")
+    void restarSaldoDestino(String numeroCuenta, int valor);
+
+    //Transferir
+    
+    @Query("{ 'numeroCuenta': ?0 }")
+    @Update("{$push: { 'operaciones_cuenta': {_id: 5, tipo: 'transferir', cuenta_afectada: ?0, valor: ?2, fecha: new Date(), punto_atencion: [] }}, $inc: { 'saldo': ?2 }, $set: { 'fecha_ultima_transaccion': new Date() }}")
+    void restarSaldoTransferir(String numeroCuenta, String numeroCuenta2, int valor);
+
+    @Query(("{'numeroCuenta': ?1 }"))
+    @Update("{$push: { 'operaciones_cuenta': { _id: 4, tipo: 'transferir', cuenta_afectada: ?1, valor: ?2, fecha: new Date(), punto_atencion: []}}, $inc: { 'saldo': ?2 }, $set: { 'fecha_ultima_transaccion': new Date() }})")
+    void sumarSaldoTransferir(String numeroCuenta, String numeroCuenta2, int valor);
 }
